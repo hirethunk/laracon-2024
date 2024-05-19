c7451224552a95ea536eb705d4e69e644c0905ac
Jacob Davis <jacobamdavis@gmail.com>
2024-05-19 16:01:08 -0400

    remove examples

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,23 +23,5 @@
                 </div>
             </div>
         </div>
-
-        <div class="p-20 h-10 w-1/2 bg-black flex justify-between items-center">
-            <span class="p-8 border-gold-100 text-white">Border Gold 100</span>
-            <span class="text-gold-100">Text Gold 100</span>
-            <div class="p-10 bg-gold-100 text-black">BG Gold 100</div>
-        </div>
-
-        <div class="p-20 h-10 w-1/2 bg-black flex justify-between items-center">
-            <span class="p-8 border-gold-500 text-white">Border Gold 500</span>
-            <span class="text-gold-500">Text Gold 500</span>
-            <div class="p-10 bg-gold-500 text-black">BG Gold 500</div>
-        </div>
-
-        <div class="p-20 h-10 w-1/2 bg-black flex justify-between items-center">
-            <span class="p-8 border-gold-900 text-white">Border Gold 900</span>
-            <span class="text-gold-900">Text Gold 900</span>
-            <div class="p-10 bg-gold-900 text-black">BG Gold 900</div>
-        </div>
     @endif
 </x-app-layout>
