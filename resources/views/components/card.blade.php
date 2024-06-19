0b2ba62b9fdda56543efcc2bef40662b44340524
Jacob Davis <jacobamdavis@gmail.com>
2024-06-19 16:58:50 -0400

    wip

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,9 @@
-<div class="p-4 sm:p-8 bg-gold-100 shadow sm:rounded-lg">
-    <div class="max-w-xl">
-        {{ $slot }}
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="text-white border-gold-100 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6">
+                {{ $slot }}
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,22 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-black inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+@props([
+    'color' => 'black',
+])
+
+@php
+switch ($color) {
+    case 'black':
+        $bgColor = 'bg-black';
+        $textColor = 'text-white';
+        $hover = 'hover:bg-gray-700';
+        break;
+    case 'gold':
+        $bgColor = 'bg-gold-500';
+        $textColor = 'text-black';
+        $hover = 'hover:bg-gold-100';
+        break;
+}
+@endphp
+
+<button {{ $attributes->merge(['type' => 'submit', 'class' => "$bgColor $textColor $hover inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"]) }}>
     {{ $slot }}
 </button>
