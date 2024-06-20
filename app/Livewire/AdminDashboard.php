e59469bf974e1d0182af0c051c8faea6edebff84
Jacob Davis <jacobamdavis@gmail.com>
2024-06-20 18:52:47 -0400

    add admin dashboard

---
+++ app/Livewire/AdminDashboard.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+use Livewire\Attributes\Computed;
+use Illuminate\Support\Facades\Auth;
+use App\Events\AdminApprovedNewPlayer;
+
+class AdminDashboard extends Component
+{
+    public ?int $user_id;
+
+    #[Computed]
+    public function unapprovedUsers()
+    {
+        return User::unapproved()->get();
+    }
+
+    #[Computed]
+    public function options()
+    {
+        return $this->unapprovedUsers->mapWithKeys(fn($user) => [$user->id => $user->name]);
+    }
+
+    public function approve()
+    {
+        if ($this->user_id === null) {
+            return;
+        }
+
+        AdminApprovedNewPlayer::fire(
+            admin_id: Auth::user()->id,
+            user_id: $this->user_id,
+            game_id: game()->id,
+            player_id: null,
+        );
+
+        return redirect()->route('admin-dashboard');
+    }
+
+    public function render()
+    {
+        return view('livewire.admin-dashboard')->layout('layouts.app');
+    }
+}
