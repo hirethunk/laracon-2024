ce4c8efa69e8d83260432b67c275a9da4a8e4d16
Jacob Davis <jacobamdavis@gmail.com>
2024-06-20 18:52:11 -0400

    add admin middleware

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Livewire\PlayerProfile;
+use App\Livewire\AdminDashboard;
 use App\Livewire\PlayerDashboard;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
-use App\Livewire\PlayerProfile;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,6 +22,10 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/player-dashboard', PlayerDashboard::class)->name('player-dashboard');
     Route::get('/players/{player}/profile', PlayerProfile::class)->name('player.profile');
+});
+
+Route::middleware('admin')->group(function () {
+    Route::get('/admin-dashboard', AdminDashboard::class, 'admin')->name('admin-dashboard');
 });
 
 require __DIR__.'/auth.php';
