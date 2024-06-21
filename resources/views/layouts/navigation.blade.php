9e9e3a1c5cf1ce82be6a8d2536039bf0fdf0c3c7
Jacob Davis <jacobamdavis@gmail.com>
2024-06-21 14:45:52 -0400

    wip

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,7 +30,7 @@
                 <x-flash.fired />
             </div>
 
-            <div class="absolute inset-0 top-1 z-0">
+            <div class="absolute inset-x-0 top-1 z-0">
                 <x-flash.fired />
             </div>
 
