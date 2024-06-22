6bf7658b9ee7f8ddef6801f9b71adcfd095f9858
Jacob Davis <jacobamdavis@gmail.com>
2024-06-22 16:37:50 -0400

    create radius for gold borders

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
-        <div class="text-white border-gold-100 overflow-hidden shadow-sm rounded-lg">
+    <div class="mx-auto">
+        <div class="border-gold-100">
             <div class="p-6">
                 {{ $slot }}
             </div>
