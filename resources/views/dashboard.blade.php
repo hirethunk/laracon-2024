9eb2ef4622cf803872b4f01a3d001f41292d3185
Jacob Davis <jacobamdavis@gmail.com>
2024-06-23 18:13:25 -0400

    dashboard links to profile page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,25 @@
 <x-app-layout>
     <x-slot name="header" class="text-gold-500">
-        <h2 class="text-8xl font-bold leading-tight text-center cinzel text-gold-500">
+        <h2 class="font-bold leading-tight text-center text-8xl cinzel text-gold-500">
             {{ __("let's make some money") }}
         </h2>
     </x-slot>
 
     <div class="py-12 bg-black">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden text-lg font-bold text-center border-y-2 border-white cardo-bold text-gold-100">
-                <p class="p-6">
+            <div class="flex flex-col py-6 overflow-hidden text-lg font-bold text-center border-white border-y-2 cardo-bold gap-y-6">
+                <p class="text-gold-100">
                     Before you can join the game, the man with the golden suit must approve your account.
                 </p>
-                <p class="px-6">Find the man with the golden suit.</p>
-                <p class="p-6">
-                    He will not approve you unless your account name matches the name on your Laracon badge. If you need to change your name to match your badge, do that below.
+                <p class="text-gold-100">
+                    Find the man with the golden suit.
+                </p>
+                <p>
+                    <span class="text-gold-100">
+                        He will not approve you unless your account name matches the name on your Laracon badge.
+                        You may change your name on the
+                    </span>
+                    <a href="/profile" class="text-indigo-400">Profile Page</a><span>.</span>
                 </p>
             </div>
         </div>
