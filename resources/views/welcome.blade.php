680942db4bec7bfe4c4303228165205c8acfe953
Jacob Davis <jacobamdavis@gmail.com>
2024-07-07 20:06:50 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,19 @@
 <x-guest-layout>
     <div class="flex flex-col space-y-4">
-    <p>TBD make this page good.</p>
+        <p>TBD make this page good.</p>
 
-    @if(Auth::user())
-        <a href="{{ route('login') }}">
-            <x-primary-button>
-                Login
-            </x-primary-button>
-        </a>
-    @else
-        <a href="{{ route('dashboard') }}">
-            <x-primary-button>
-                Dashboard
-            </x-primary-button>    
-        </a>
-    @endif
-
+        @if(Auth::user())
+            <a href="{{ route('login') }}">
+                <x-primary-button>
+                    Login
+                </x-primary-button>
+            </a>
+        @else
+            <a href="{{ route('dashboard') }}">
+                <x-primary-button>
+                    Dashboard
+                </x-primary-button>
+            </a>
+        @endif
+    </div>
 </x-guest-layout>
