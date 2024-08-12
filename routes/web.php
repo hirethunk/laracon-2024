75fe2ebfa2bcbe2315c98470bb377cd5961e5a74
Jacob Davis <jacobamdavis@gmail.com>
2024-08-12 01:18:04 -0400

    remove foo

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/foo', function () {
-    return view('foo');
-});
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
