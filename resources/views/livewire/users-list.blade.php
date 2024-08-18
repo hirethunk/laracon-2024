a75cd4ef01dc3ba3516179261b00b4c4b8e952d6
Jacob Davis <jacobamdavis@gmail.com>
2024-08-18 13:52:10 -0400

    Update users-list.blade.php

--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -1,13 +1,13 @@
 <div class="p-4 max-w-80 mx-auto bg-white">
-    <lwa::autocomplete>
-        <lwa::autocomplete-input class="p-4 text-red-500" />
+    <x-autocomplete>
+        <x-autocomplete-input class="p-4 text-red-500" />
 
-        <lwa::autocomplete-list class="text-blue-400">
+        <x-autocomplete-list class="text-blue-400 max-h-56">
             @foreach ($this->users as $user)
-                <lwa::autocomplete-item>
+                <x-autocomplete-item>
                     {{ $user->name }}
-                </lwa::autocomplete-item>
+                </x-autocomplete-item>
             @endforeach
-        </lwa::autocomplete-list>
-    </lwa::autocomplete>
+        </x-autocomplete-list>
+    </x-autocomplete>
 </div>
