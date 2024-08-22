8c490819a6c2e42c90e97b646e004aa5f01ecf3d
John Drexler <johnrudolphdrexler@gmail.com>
2024-08-21 22:52:59 -0400

    WIP

--- tests/Feature/PlayerProfileTest.php
+++ tests/Feature/PlayerProfileTest.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Events\PlayerEnteredSecretCode;
 use App\Livewire\PlayerProfile;
-use App\Livewire\SecretCodePage;
 use App\Models\Game;
-use Illuminate\Support\Carbon;
 use Livewire\Livewire;
 use Thunk\Verbs\Facades\Verbs;
 
@@ -16,7 +13,7 @@ beforeEach(function () {
     $this->game = Game::first();
 });
 
-it('allows player to submit code in livewire component', function () {
+it('renders the score history', function () {
     $this->actingAs($this->taylor->user);
 
     Livewire::test(PlayerProfile::class, ['player' => $this->taylor])
