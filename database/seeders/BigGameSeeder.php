768d8d6b7d26e8f9abb2ef54270c89b1c47fac8b
Chris Morrell <inxilpro@users.noreply.github.com>
2024-08-25 14:40:42 -0400

    wip

--- database/seeders/BigGameSeeder.php
+++ database/seeders/BigGameSeeder.php
@@ -20,11 +20,13 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Date;
 use Thunk\Verbs\Exceptions\EventNotValidForCurrentState;
 use Thunk\Verbs\Facades\Verbs;
+use Thunk\Verbs\Lifecycle\Queue;
+
 use function Laravel\Prompts\progress;
 
 class BigGameSeeder extends Seeder
 {
-    protected const PLAYER_COUNT = 50;
+    protected const PLAYER_COUNT = 500;
 
     protected int $game_id;
 
@@ -38,6 +40,8 @@ class BigGameSeeder extends Seeder
 
     protected Collection $unused_codes;
 
+    protected Queue $queue;
+
     public function run(): void
     {
         // $GLOBALS['info'] = $this->command->line(...);
@@ -46,8 +50,8 @@ class BigGameSeeder extends Seeder
         // Event::listen(function(QueryExecuted $e) {
         // 	$this->command->warn($e->toRawSql());
         // });
-	    
-	    $this->command->newLine();
+
+        $this->command->newLine();
 
         $this->setup();
         $this->createGame();
@@ -57,14 +61,15 @@ class BigGameSeeder extends Seeder
             $this->playRound();
             Date::setTestNow(now()->addHour());
         }
-	    
-	    $this->command->newLine();
+
+        $this->command->newLine();
     }
 
     protected function setup(): void
     {
         ini_set('memory_limit', '-1');
 
+        $this->queue = app(Queue::class);
         $this->game_id = snowflake_id();
         $this->admin_id = snowflake_id();
 
@@ -73,9 +78,9 @@ class BigGameSeeder extends Seeder
 
     protected function createGame(): void
     {
-	    $progress = progress('Creating game...', 4);
-	    $progress->start();
-		
+        $progress = progress('Creating game...', 4);
+        $progress->start();
+
         GameCreated::fire(
             game_id: $this->game_id,
             name: 'Laracon 2024',
@@ -83,8 +88,8 @@ class BigGameSeeder extends Seeder
         );
 
         $this->game = GameState::load($this->game_id);
-		
-		$progress->advance();
+
+        $progress->advance();
 
         UserCreated::fire(
             user_id: $this->admin_id,
@@ -92,19 +97,19 @@ class BigGameSeeder extends Seeder
             email: 'admin@admin.com',
             password: bcrypt('password'),
         );
-		
-	    $progress->advance();
+
+        $progress->advance();
 
         UserPromotedToAdmin::fire(
             user_id: $this->admin_id,
             game_id: $this->game_id,
         );
-	    
-	    $progress->advance();
+
+        $progress->advance();
 
         Verbs::commit();
-	    
-	    $progress->advance();
+
+        $progress->advance();
     }
 
     protected function createPlayers(): void
@@ -112,9 +117,9 @@ class BigGameSeeder extends Seeder
         $faker = fake();
         $user_ids = [];
         $player_ids = [];
-		
-		$progress = progress('Creating players...', self::PLAYER_COUNT);
-		$progress->start();
+
+        $progress = progress('Creating players...', self::PLAYER_COUNT);
+        $progress->start();
 
         for ($i = 0; $i < self::PLAYER_COUNT; $i++) {
             $user_id = snowflake_id();
@@ -151,74 +156,79 @@ class BigGameSeeder extends Seeder
 
             $user_ids[] = $user_id;
             $player_ids[] = $player_id;
-			
-			$progress->advance();
+
+            $progress->advance();
         }
 
         $this->users = collect($user_ids);
         $this->players = collect($player_ids);
 
         Verbs::commit();
-		
-		$progress->finish();
+
+        $progress->finish();
     }
-	
-	protected function playRound(): void
-	{
-		$progress = progress('Playing '.now()->format('h:ia').' round...', $this->players->count());
-		$progress->start();
-		
-		foreach ($this->players as $index => $player_id) {
-			$other_player_ids = $this->players->random(3)->filter(fn($id) => $id !== $player_id);
-			
-			if ($other_player_ids->count() < 2) {
-				break;
-			}
-			
-			try {
-				PlayerVoted::fire(
-					game_id: $this->game_id,
-					player_id: $player_id,
-					upvotee_id: $other_player_ids->pop(),
-					downvotee_id: $other_player_ids->pop(),
-				);
-				
-				// Maybe play secret code
-				if (random_int(1, 10) === 1 && $this->unused_codes->isNotEmpty()) {
-					$code = $this->unused_codes->pop();
-					
-					// most people play it just once, 1 in 10 play it 10+ times
-					$plays = random_int(1, 10) === 1 ? random_int(10, 50) : 1;
-					
-					for ($i = 0; $i < $plays; $i++) {
-						PlayerEnteredSecretCode::fire(
-							player_id: $player_id,
-							game_id: $this->game_id,
-							secret_code: $code,
-						);
-					}
-				}
-				
-				// Maybe resign
-				if ($other_player_ids->isNotEmpty() && random_int(1, round(static::PLAYER_COUNT / 2)) === 1) {
-					PlayerResigned::fire(
-						player_id: $player_id,
-						beneficiary_id: $other_player_ids->pop(),
-						game_id: $this->game_id,
-					);
-					$this->players->forget($index);
-				}
-			} catch (EventNotValidForCurrentState) {
-				// just ignore
-			} catch (AuthorizationException) {
-				// also just ignore
-			}
-			
-			$progress->advance();
-		}
-		
-		Verbs::commit();
-		
-		$progress->finish();
-	}
+
+    protected function playRound(): void
+    {
+        $progress = progress('Playing '.now()->format('h:ia').' round...', $this->players->count());
+        $progress->start();
+
+        foreach ($this->players as $index => $player_id) {
+            if (count($this->queue->event_queue) >= 100) {
+                Verbs::commit();
+            }
+
+            $other_player_ids = $this->players->random(5)->filter(fn ($id) => $id !== $player_id);
+
+            if ($other_player_ids->count() < 2) {
+                $progress->advance();
+                break;
+            }
+
+            try {
+                PlayerVoted::fire(
+                    game_id: $this->game_id,
+                    player_id: $player_id,
+                    upvotee_id: $other_player_ids->pop(),
+                    downvotee_id: $other_player_ids->pop(),
+                );
+
+                // Maybe play secret code
+                if (random_int(1, 10) === 1 && $this->unused_codes->isNotEmpty()) {
+                    $code = $this->unused_codes->pop();
+
+                    // most people play it just once, 1 in 10 play it 10+ times
+                    $plays = random_int(1, 10) === 1 ? random_int(10, 50) : 1;
+
+                    for ($i = 0; $i < $plays; $i++) {
+                        PlayerEnteredSecretCode::fire(
+                            player_id: $player_id,
+                            game_id: $this->game_id,
+                            secret_code: $code,
+                        );
+                    }
+                }
+
+                // Maybe resign
+                if ($other_player_ids->isNotEmpty() && random_int(1, round(static::PLAYER_COUNT / 2)) === 1) {
+                    PlayerResigned::fire(
+                        player_id: $player_id,
+                        beneficiary_id: $other_player_ids->pop(),
+                        game_id: $this->game_id,
+                    );
+                    $this->players->forget($index);
+                }
+            } catch (EventNotValidForCurrentState) {
+                // just ignore
+            } catch (AuthorizationException) {
+                // also just ignore
+            }
+
+            $progress->advance();
+        }
+
+        Verbs::commit();
+
+        $progress->finish();
+    }
 }
