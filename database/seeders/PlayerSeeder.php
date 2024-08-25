c24e3946ce0b14ae827c399d5e8cf239e28cf3fa
Jacob Davis <jacobamdavis@gmail.com>
2024-08-25 19:11:57 -0400

    Make user and player seeders for UI Testing

--- database/seeders/PlayerSeeder.php
+++ database/seeders/PlayerSeeder.php
@@ -2,15 +2,42 @@
 
 namespace Database\Seeders;
 
+use App\Models\Game;
+use App\Models\User;
+use App\Events\UserCreated;
 use Illuminate\Database\Seeder;
+use App\Events\AdminApprovedNewPlayer;
 
 class PlayerSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    protected $admin_id;
+    protected $game_id;
+
+    public function __construct()
+    {
+        $this->admin_id = User::first()->id;
+        $this->game_id = Game::first()->id;
+    }
+
     public function run(): void
     {
-        //
+        $this->createPlayer(300);
+    }
+
+    protected function createPlayer(int $count): void
+    {
+        for ($i = 1; $i <= $count; $i++) {
+            $user_id = UserCreated::fire(
+                name: 'Test User'.$i,
+                email: 'test'.$i.'@thunk.dev',
+                password: bcrypt('password'),
+            )->user_id;
+
+            AdminApprovedNewPlayer::fire(
+                admin_id: $this->admin_id,
+                user_id: $user_id,
+                game_id: $this->game_id,
+            );
+        }
     }
 }
