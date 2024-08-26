cafb640aced6f212fcd72861226cd9fbec542e75
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2024-08-26 18:37:02 -0500

    WIP

--- app/Console/Commands/SetupAdmins.php
+++ app/Console/Commands/SetupAdmins.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands;
 
-use App\Events\UserCreated;
+use App\Models\User;
 use Glhd\Bits\Snowflake;
-use Illuminate\Console\Command;
+use App\Events\UserCreated;
+use App\Events\UserPromotedToAdmin;
+use App\Models\Game;
 use Thunk\Verbs\Facades\Verbs;
+use Illuminate\Console\Command;
 
 class SetupAdmins extends Command
 {
@@ -15,35 +18,47 @@ class SetupAdmins extends Command
 
     public function handle()
     {
-        UserCreated::fire(
+
+        $john = User::firstWhere('email', 'john@thunk.dev')?->id ?? UserCreated::fire(
             name: 'John Drexler',
             email: 'john@thunk.dev',
             password: bcrypt(Snowflake::make()->id()),
-        );
+        )->user_id;
 
-        UserCreated::fire(
+        $daniel = User::firstWhere('email', 'daniel@thunk.dev')?->id ?? UserCreated::fire(
             name: 'Daniel Coulbourne',
             email: 'daniel@thunk.dev',
             password: bcrypt(Snowflake::make()->id()),
         )->user_id;
 
-        UserCreated::fire(
+        $jacob = User::firstWhere('email', 'jacob@thunk.dev')?->id ?? UserCreated::fire(
             name: 'Jacob Davis',
             email: 'jacob@thunk.dev',
             password: bcrypt(Snowflake::make()->id()),
-        );
+        )->user_id;
 
-        UserCreated::fire(
+        $josh = User::firstWhere('email', 'josh@thunk.dev')?->id ?? UserCreated::fire(
             name: 'Josh Hanley',
             email: 'josh@thunk.dev',
             password: bcrypt(Snowflake::make()->id()),
-        );
+        )->user_id;
 
-        UserCreated::fire(
+        $chris = User::firstWhere('email', 'chris@nachi.org')?->id ?? UserCreated::fire(
             name: 'Chris Morrell',
             email: 'chris@nachi.org',
             password: bcrypt(Snowflake::make()->id()),
-        );
+        )->user_id;
+
+        $admins = [$john, $daniel, $jacob, $josh, $chris];
+
+        Game::all()->each(function ($g) use ($admins) {
+            foreach ($admins as $admin) {
+                UserPromotedToAdmin::fire(
+                    user_id: $admin,
+                    game_id: $g->id,
+                );
+            }
+        });
 
         Verbs::commit();
     }
